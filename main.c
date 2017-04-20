556b6cebb782b3a9f731a673dacf35e7d9d2cce6
Swhite <1779011982@qq.com>
2017-04-20 21:17:29 +0800

    主函数功能实现

--- main.c
+++ main.c
@@ -1,26 +1,32 @@
 #include "mycom.h"
+
 extern DWORD num;
-extern const unsigned char *cmd[];
-extern const unsigned char *post[];
+extern unsigned char *cmd[];    //AT命令
+extern unsigned char *post[];    //post数据包
 extern BYTE gbuff[512];
 extern BYTE rbuff[512];
+
+const unsigned char *device_id = "&device=device007";     //每个终端(树莓派)固定的设备号
+
 /**************************************************
-主函数 : main()
-参数   : void
-返回值 : int
-功能   : 初始化rfid和gprs模块的接口  \
+ *主函数 : main()
+ *参数   : void
+ *返回值 : int
+ *功能   : 初始化rfid和gprs模块的接口  \
 		 循环读取rfid卡号，缓冲区有数据时调用gprs()
-***************************************************/
+ ***************************************************/
 int main(void)
 {
 	int rfid_fd = -1;
 	int gprs_fd = -1;
 	int flag = 0;
+
+	/*初始化操作*/
 	rfid_fd = rfid_init();
 	gprs_fd = gprs_init();
 
+
 	/*循环读卡*/
-	
 	while(1)
 	{
 		flag = rfid(rfid_fd);
@@ -28,28 +34,27 @@ int main(void)
 		{
 			printf("\n\n等待GPRS执行模块......\n\n");
 			gprs(gprs_fd);
-			printf("本次签到完成！！！");
+			printf("本次签到完成！！！\n\n");
 		}
 	}
-	
 	return 0;
 }
 
 
 /*************************************************
-函数名 : gprs()
-参数   : int fd
-返回值 : void
-功能   : 发送初始化指令(为了节省流量每次执行后都关
+ *函数名 : gprs()
+ *参数   : int fd
+ *返回值 : void
+ *功能   : 发送初始化指令(为了节省流量每次执行后都关
 		 闭TCP链接),建立TCP链接，发送POST数据包
-*************************************************/
+ *************************************************/
 void gprs(int fd)
 {
 	int w_ret,r_ret;
 	int i,j;
+	char over_flag = 0x1a;
 
-
-	for(i = 7;i < 8;i++)
+	for(i = 7;i < 9;i++)
 	{
 		memset(gbuff, '\0', num);
 		w_ret = com_write(fd, cmd[i], strlen(cmd[i]));
@@ -73,8 +78,11 @@ void gprs(int fd)
 		printf("GPRS写入命令：%s\n",post[j]);
 		printf("GPRS写入字节数：%d\n",w_ret);
 	}
-	sleep(1);
-	printf("发送成功！");
+	com_write(fd, &over_flag, sizeof(over_flag));   //发送0X1A结束指令
+	
+
+	sleep(2);
+	printf("发送中POST数据包中......\n\n");
 	r_ret = com_read(fd, gbuff, num);
 	printf("服务器返回数据：%s\n",gbuff);
 	printf("返回字节数：%d\n",r_ret);
@@ -83,31 +91,76 @@ void gprs(int fd)
 	/*关闭TCP链接*/
 	memset(gbuff, '\0', num);
 	com_write(fd, cmd[9], strlen(cmd[9]));
+	sleep(2);
 	r_ret = com_read(fd, gbuff, num);
-	printf("%s\n",gbuff);
+	printf("关闭：%s\n",gbuff);
 }
 
 
 /**************************************************
- *函数名：rfid()
- *功  能：读卡，将卡号打印出来
- *参  数：fd
- *返回值：int型数组
-**************************************************/
+ *函数名 : rfid()
+ *功  能 : 读卡，将卡号打印出来
+ *参  数 : int fd
+ *返回值 : int型,卡号位数(用于辨别是否有读到卡)
+ *************************************************/
 int rfid(int fd)
 {
-	memset(rbuff, '0', num);
 	int ret;
-	int i;
+	int i,j;
+	char tmp;
 
-	sleep(2);
+
+	memset(rbuff, '\0', num);
+	sleep(1);
 	ret = com_read(fd, rbuff, num);
 	printf("RFID接收字节数：%d\n",ret);
 	printf("卡号：");
-	for(i = 0;i < ret;i++)
+
+
+	/*将卡号转换为字符串，并将地址存在指针id中*/
+	for(j = 0;j < ret;j++)
 	{
-		printf("%d",rbuff[i]);
+		tmp = (((int)rbuff[j])%3) + (48 + j%2);
+		rbuff[j] = tmp;
+	}
+
+
+	/*打印卡号,并将卡号填充到POST数据包*/
+	if(ret > 0)
+	{
+		printf("%s\n",rbuff);
+		card_to_post(rbuff);
+	}
+	else
+	{
+		printf("********请刷卡********");
 	}
 	printf("\n");
 	return ret;
+}
+
+
+/*********************************************
+ *函数名 : card_to_post()
+ *功能   : 将卡号填充到POST数据包
+ *参数   : unsigned char card[]，卡号字符串数组地址
+ *返回值 : void
+ **********************************************/
+void card_to_post(unsigned char card[])
+{
+	/*.................各种拼接*/
+	unsigned char part_1[50] = "key=iot&location=";
+	unsigned char part_2[50];
+	strcpy(part_2, card);
+	unsigned char part_3[100];
+	strcpy(part_3, device_id);
+	unsigned char part_1_2[100];
+	strcpy(part_1_2, strcat(part_1, part_2));
+	static unsigned char part_1_2_3[100];
+	memset(part_1_2_3, '\0', 100);
+	strcpy(part_1_2_3, strcat(part_1_2, part_3));
+
+
+	/*将最终拼接结果字符串地址赋给post[4]*/
+	post[4] = part_1_2_3;
 }
