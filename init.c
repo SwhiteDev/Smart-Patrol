af826013124110a9e03fc5da6dbfb96d78f40679
Swhite <1779011982@qq.com>
2017-04-20 21:16:06 +0800

    初始化RFID和GPRS相关

--- init.c
+++ init.c
@@ -1,43 +1,45 @@
 #include "mycom.h"
 
-const  unsigned char device_id;    /*每个设备固定的设备号*/
 unsigned char rbuff[512];   /*rfid读取缓冲区*/
 unsigned char gbuff[512];   /*gprs读取缓冲区*/
-unsigned short num;          /*读取和写入字节*/
+unsigned short num = 512;   /*读取和写入字节*/
 
 
 /****命令缓冲区，cmd为初始化AT指令，post为post数据包****/
-const char *cmd[] = {
-								"AT\r", 				 \
-								"AT+CCID\r", 				\
-								"AT+CSQ\r", 				\
-								"AT+CGCLASS=\"B\"\r", 				\
-								"AT+CGDCONT=1,\"IP\",\"CMNET\"\r", 				\
-								"AT+CGATT=1\r", 				\
-								"AT+CIPCSGP=1,\"CMNET\"\r", 				\
-								"AT+CIPSTART=\"TCP\",\"182.254.244.212\",\"80\"\r", 				\
-								"AT+CIPSEND=181\r", 				\
-								"AT+CIPCLOSE\r" 				\
-					   		 };
+unsigned char *cmd[] = {
+						"AT\r", 				 \
+						"AT+CCID\r", 				\
+						"AT+CSQ\r", 				\
+						"AT+CGCLASS=\"B\"\r", 				\
+						"AT+CGDCONT=1,\"IP\",\"CMNET\"\r", 				\
+						"AT+CGATT=1\r", 				\
+						"AT+CIPCSGP=1,\"CMNET\"\r", 				\
+						"AT+CIPSTART=\"TCP\",\"182.254.244.212\",\"80\"\r", 				\
+						"AT+CIPSEND\r", 				\
+						"AT+CIPCLOSE\r" 				\
+	 				   };
 
-const char *post[] = {"POST /xg/index.php/admin/upload/record\r\n", 				\
-							   "Host: www.hicoder.cn\r\n", 				\
-						       "Content-Type:application/x-www-form-urlencoded\r\n", 				\
-						   	   "Content-Length:37\r\n", 				\
-							   "\r\nkey=iot&location=002&device=device001"  				\
-							  };
+unsigned char *post[] = {
+				   		 "POST /xg/index.php/admin/upload/record HTTP/1.1\r\n", 				\
+			   	  		 "Host: www.hicoder.cn\r\n", 				\
+			       		 "Content-Type: application/x-www-form-urlencoded\r\n", 				\
+			   	    	 "Content-Length: 42\r\n\r\n", 				\
+				   		 "key=iot&location=002&device=device001"  				\
+			   			};
+
 
 /**************************************************
  *函数名：rfid_init()
  *功  能：初始化rfid模块接口
  *参  数：void
- *返回值：int
+ *返回值：int fd
 **************************************************/
 int rfid_init(void)
 {
 	const char *dev = "/dev/ttyUSB0";
 	int fd = com_open(dev);
 
+	/*设置波特率乱七八糟的*/
 	com_attr attr = {0};
 	attr.baudrate = 19200;
 	attr.databits = 8;
@@ -49,18 +51,20 @@ int rfid_init(void)
 	return fd;
 }
 
+
 /**************************************************
  *函数名：gprs_init()
  *功  能：初始化gprs模块接口，与服务器建立tcp链接
  *参  数：void
- *返回值：int
+ *返回值：int fd
 **************************************************/
 int gprs_init()
 {
 	int r_ret, w_ret, i;
 	char *dev = "/dev/ttyUSB1";
 	int fd = com_open(dev);
-
+	
+	/*设置波特率乱七八糟的*/
 	com_attr attr = {0};
 	attr.baudrate = 9600;
 	attr.databits = 8;
@@ -72,7 +76,7 @@ int gprs_init()
 
 
 	/**建立链接**/
-	for(i = 0;i < 9;i++)
+	for(i = 0;i < 7;i++)
 	{
 		memset(gbuff, '\0', num);
 		w_ret = com_write(fd, cmd[i], strlen(cmd[i]));
