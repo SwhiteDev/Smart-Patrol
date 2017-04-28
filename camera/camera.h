e172d0f0a1e54dd75b5e91fcbc4f8e86430fd676
Swhite <1779011982@qq.com>
2017-04-28 14:43:10 +0800

    新文件：   Makefile 	新文件：   camera 	修改：     camera.c 	修改：     camera.h 	修改：     cscope.in.out 	修改：     cscope.out 	修改：     cscope.po.out 	修改：     tags
    	修改：     ../Makefile

--- camera/camera.h
+++ camera/camera.h
@@ -1,7 +1,19 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <termios.h>
+#include <linux/videodev2.h>
+
 #define TRUE 1
 #define FALSE 0
 
-#define FILE_VIDEO "/dev/video0"
+#define FILE_VIDEO "/dev/video2"
 #define BMP 	   "/home/swhite/Smart-Patrol/camera/picture/image.bmp"
 #define YUV 	   "/home/swhite/Smart-Patrol/camera/picture/image.yuv"
 
@@ -9,11 +21,11 @@
 #define IMAGEHEIGHT 480
 
 static int fd_video;
-static static v4l2_capability cap;
-struct v4l2_fmtdesc fmtdesc;
-struct v4l2_format fmt,fmtack;
+static struct v4l2_capability cap;    //设备属性
+struct v4l2_fmtdesc fmtdesc;          //帧格式
+struct v4l2_format fmt,fmtack;        
 struct v4l2_streamparm setfps;
-struct v4l2_requestbuffers req;
+struct v4l2_requestbuffers req;       //申请缓冲区
 struct v4l2_buffer buf;
 enum v4l2_buf_type type;
 
